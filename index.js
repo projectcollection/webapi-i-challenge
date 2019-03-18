f1beb6792859d4b3515a1de686187b2e09191d50
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-03-18 15:00:33 -0400

    setup routes

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// implement your API here
+const express = require('express')
+const db = require('./data/db')
+
+const base = '/api/users'
+const app = express()
+
+app.use(express.json())
+
+app.get(`${base}`, (req, res) => {
+    res.send('hello world')
+})
+
+app.get(`${base}/:id`, (req, res) => {
+    res.send('hello world')
+})
+
+app.post(`${base}`,(req, res) => {
+    res.send('hello world')
+})
+
+app.put(`${base}/:id`, (req, res) => {
+    res.send('hello world')
+})
+
+app.delete(`${base}/:id`, (req, res) => {
+    res.send('hello world')
+})
+
+const port = 5000
+
+app.listen(port, () => console.log(`running on port ${port}`))
