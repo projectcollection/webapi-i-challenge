e7290839c8e93c494a1f14c4c6ab945dc4232977
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-03-18 15:31:12 -0400

    getById done.

--- index.js
+++ index.js
@@ -8,16 +8,23 @@ app.use(express.json())
 
 app.get(`${base}`, (req, res) => {
     db.find().then(dbres => {
-        console.log(dbres)
         res.status(200).json(dbres)
     }).catch(err => {
-        console.log(err)
         res.status(500).json({ error: "There was an error while saving the user to the database" })
     })
 })
 
 app.get(`${base}/:id`, (req, res) => {
-    res.send('hello world')
+    const {id} = req.params
+    console.log(id)
+
+    db.findById(id).then(dbres => {
+        if(!dbres) res.status(404).json({ message: "The user with the specified ID does not exist." }) 
+
+        res.json(dbres)
+    }).catch(err => {
+        res.status(500).json({ error: "The user information could not be retrieved." })
+    })
 })
 
 app.post(`${base}`,(req, res) => {
